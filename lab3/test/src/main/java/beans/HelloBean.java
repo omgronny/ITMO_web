5748fbe24959f0016bf4b0c66607c31164c4bf82
omgronny <glinskikh.roman@yandex.ru>
2021-11-18 08:47:39 +0300

    scope fix

--- lab3/test/src/main/java/beans/HelloBean.java
+++ lab3/test/src/main/java/beans/HelloBean.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 @ManagedBean
-@ApplicationScoped
+@SessionScoped
 public class HelloBean  {
 
     private ValidPoint validPoint = new ValidPoint();
